295ae446c01e03574132192eaaa233b8e4a40f1e
Galdoba <galdoba@mail.ru>
2025-07-28 19:41:30 +1000

    test fix

--- logger_test.go
+++ logger_test.go
@@ -13,15 +13,14 @@ func TestLog(t *testing.T) {
 			WithTimePrecision(3),
 			WithColor(),
 		)))
-	logg.Emergency("this is %v", "EMERGENCY")
-	logg.Fatal("this is %v", "FATAL")
-	logg.Alert("this is %v", "ALERT")
-	logg.Critical("this is %v", "CRITICAL")
-	logg.Error("this is %v", "ERROR")
-	logg.Warn("this is %v", "WARN")
-	logg.Notice("this is %v", "NOTICE")
-	logg.Info("this is %v", "INFO")
-	logg.Debug("this is %v", "DEBUG")
-	logg.Trace("this is %v", "TRACE")
-
+	logg.Emergencyf("this is %v", "EMERGENCY")
+	logg.Alertf("this is %v", "ALERT")
+	logg.Criticalf("this is %v", "CRITICAL")
+	logg.Errorf("this is %v", "ERROR")
+	logg.Warnf("this is %v", "WARN")
+	logg.Noticef("this is %v", "NOTICE")
+	logg.Infof("this is %v", "INFO")
+	logg.Debugf("this is %v", "DEBUG")
+	logg.Tracef("this is %v", "TRACE")
+	logg.Fatalf("this is %v", "FATAL")
 }
